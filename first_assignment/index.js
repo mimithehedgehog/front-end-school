468571479bda810efa26a8a3b6fd2ec474fb5323
mimithehedgehog <grosu.nicoleta96@gmail.com>
2018-08-06 11:54:04 +0300

    Update index.js

--- first_assignment/index.js
+++ first_assignment/index.js
@@ -1,5 +1,3 @@
-//let sqColor = document.getElementById("culori");
-//let clickIt = document.getElementById("myButton");
 let rectangle_color = "blue";
 let button = document.getElementById ("myButton");
 
@@ -14,9 +12,6 @@ button.addEventListener('click', (event) => {
     }
     rectangle.style.backgroundColor =  rectangle_color;
 })
-//let myRectangle = document.getElementById("culori");
-//console.log(myRectangle.style.backgroundColor + "ceva");
-
 
 
 
